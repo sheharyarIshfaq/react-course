c351bbad9eed5be0a804fe8ae0c22df781149931
Sheharyar <sheharyarishfaq@gmail.com>
2021-08-23 16:24:35 +0500

    Animating React Apps Section Completed

---
+++ animating_react_apps/src/components/Modal/Modal.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { CSSTransition } from "react-transition-group";
+// import Transition from "react-transition-group/Transition";
+
+import "./Modal.css";
+
+const animationTiming = {
+  enter: 400,
+  exit: 1000,
+};
+
+const modal = (props) => {
+  return (
+    <CSSTransition
+      in={props.show}
+      mountOnEnter
+      unmountOnExit
+      timeout={animationTiming}
+      classNames={{
+        enter: '',
+        enterActive:'ModalOpen',
+        exit: '',
+        exitActive:'ModalClosed'
+      }}
+    >
+      <div className='Modal'>
+        <h1>A Modal</h1>
+        <button className="Button" onClick={props.closed}>
+          Dismiss
+        </button>
+      </div>
+    </CSSTransition>
+    // <Transition
+    //   mountOnEnter
+    //   unmountOnExit
+    //   in={props.show}
+    //   timeout={animationTiming}
+    // >
+    //   {(state) => {
+    //     const cssClasses = [
+    //       "Modal",
+    //       state === "entering"
+    //         ? "ModalOpen"
+    //         : state === "exiting"
+    //         ? "ModalClosed"
+    //         : null,
+    //     ];
+    //     return (
+    //       <div className={cssClasses.join(" ")}>
+    //         <h1>A Modal</h1>
+    //         <button className="Button" onClick={props.closed}>
+    //           Dismiss
+    //         </button>
+    //       </div>
+    //     );
+    //   }}
+    // </Transition>
+  );
+};
+
+export default modal;
